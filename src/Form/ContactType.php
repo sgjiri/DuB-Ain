72b498e4ac11e68bc5e3358d1d49d25918ba61cc
sgjiri <sg.jiri@yahoo.fr>
2024-03-20 23:12:56 +0100

    form

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -1,7 +1,9 @@
 <?php
 
+// Définir l'espace de noms pour la classe de formulaire
 namespace App\Form;
 
+// Importer les composants nécessaires de Symfony
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -11,99 +13,112 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Validator\Constraints as Assert;
 
+// Créer une classe de type de formulaire personnalisée pour un formulaire de contact
 class ContactType extends AbstractType
 {
+    // Construire le formulaire avec les champs et les contraintes de validation
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // Commencer à construire le formulaire
         $builder
-        ->add('Nom', TextType::class,[
-            'constraints' => [
-                new Assert\NotBlank(),
-                new Assert\Length(['min' => 2, 'max' => 50]),
-            ],
-            'required' => true, 
-            'attr' => [
-                'placeholder' => 'Votre nom',
-            ],
-        ])
-        ->add('Prenom', TextType::class, [
-            'constraints' => [
-                new Assert\NotBlank(),
-                new Assert\Length(['min' => 2, 'max' => 50]),
-            ],
-            'required' => true, 
-            'attr' => [
-                'placeholder' => 'Votre prénom',
-            ],
-        ])
-        ->add('email', EmailType::class, [
-             'constraints' => [
-                new Assert\NotBlank(),
-                new Assert\Email([
-                    'message' => 'L\'email n\'est pas valide.'
-                ]),
-            ],
-            'required' => true,
-            'attr' => [
-                'placeholder' => 'Votre email',
-            ],
-        ])
-        ->add('Telephone', TextType::class, [
-            'constraints' => [
-                new Assert\NotBlank([
-                    'message' => 'Le numéro de téléphone est obligatoire.',
-                ]),
-                new Assert\Length([
-                    'min' => 10,
-                    'max' => 15,
-                    'minMessage' => 'Le numéro de téléphone doit comporter au moins {{ limit }} chiffres.',
-                    'maxMessage' => 'Le numéro de téléphone ne peut pas dépasser {{ limit }} chiffres.',
-                ]),
-                new Assert\Regex([
-                    'pattern' => '/^[0-9\-\(\)\/\+\s]*$/',
-                    'message' => 'Le numéro de téléphone n\'est pas valide.',
-                ]),
-            ],
-            'required' => true,
-            'attr' => [
-                'placeholder' => 'Votre numéro de téléphone',
-            ],
-        ])
-        ->add('Object', TextType::class, [
-            'constraints' => [
-                new Assert\NotBlank(),
-                new Assert\Length(['min' => 2, 'max' => 50]),
-            ],
-            'required' => true,
-            'attr' => [
-                'placeholder' => 'Votre prénom',
-            ],
-        ])
-        ->add('Message', TextareaType::class, [
-            'constraints' => [
-                new Assert\NotBlank(),
-                new Assert\Length(['min' => 2, 'max' => 1000]),
-            ],
-            'required' => true,
-            'attr' => [
-                'placeholder' => 'Votre message',
-            ],
-        ])
-        ->add('Envoyer', SubmitType::class)
-        ->add('honeypot', TextType::class, [
-            'mapped' => false,
-            'required' => false,
-            'attr' => ['style' => 'display:none;']
-        ])
-        ;
+            // Ajouter un champ 'Nom' avec des contraintes de validation
+            ->add('Nom', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                ],
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Votre nom',
+                ],
+            ])
+            // Ajouter un champ 'Prenom' avec des contraintes de validation
+            ->add('Prenom', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                ],
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Votre prénom',
+                ],
+            ])
+            // Ajouter un champ 'email' avec des contraintes de validation spécifiques aux e-mails
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Email([
+                        'message' => 'L\'email n\'est pas valide.'
+                    ]),
+                ],
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Votre email',
+                ],
+            ])
+            // Ajouter un champ 'Telephone' avec des contraintes de validation pour le format
+            // Ajouter un champ 'Telephone' avec des contraintes de validation pour le format
+            ->add('Telephone', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le numéro de téléphone est obligatoire.',
+                    ]),
+                    new Assert\Length([
+                        'min' => 10,
+                        'max' => 10,
+                        'exactMessage' => 'Le numéro de téléphone doit comporter exactement 10 chiffres.',
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^[0-9]*$/',
+                        'message' => 'Le numéro de téléphone ne peut contenir que des chiffres.',
+                    ]),
+                ],
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Votre numéro de téléphone',
+                    
+                ],
+                'error_bubbling' => false,
+            ])
+            // Ajouter un champ 'Object' pour l'objet du message
+            ->add('Object', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                ],
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'L\'objet de votre message',
+                ],
+            ])
+            // Ajouter un champ 'Message' pour le contenu du message avec une longueur maximale
+            ->add('Message', TextareaType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 2, 'max' => 1000]),
+                ],
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Votre message',
+                ],
+            ])
+            // Ajouter un bouton de soumission pour envoyer le formulaire
+            ->add('Envoyer', SubmitType::class)
+            // Ajouter un champ 'honeypot' comme mesure anti-spam (non mappé et caché)
+            ->add('honeypot', TextType::class, [
+                'mapped' => false,
+                'required' => false,
+                'attr' => ['style' => 'display:none;']
+            ]);
     }
+
+    // Configurer les options par défaut pour le formulaire, y compris la protection CSRF
     public function configureOptions(OptionsResolver $resolver): void
-{
-    $resolver->setDefaults([
-        'csrf_protection' => true,
-        'csrf_field_name' => '_token',
-        // Vous pouvez également définir un 'csrf_token_id' unique pour ce formulaire si nécessaire
-    ]);
-}
-    
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => true, // Activer la protection CSRF
+            'csrf_field_name' => '_token', // NomDu code pour le formulaire de contact en PHP en utilisant Symfony.
+            // Vous pouvez également définir un 'csrf_token_id' unique pour ce formulaire si nécessaire
+        ]);
+    }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,44 +13,60 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+/**
+ * Type de formulaire pour l'inscription d'un utilisateur.
+ */
 class RegistrationFormType extends AbstractType
 {
+    /**
+     * Construit le formulaire d'inscription.
+     *
+     * @param FormBuilderInterface $builder Le constructeur de formulaire.
+     * @param array $options Les options du formulaire.
+     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+            ->add('email', EmailType::class)
+            ->add('RGPDConsent', CheckboxType::class, [
                 'mapped' => false,
+                // 'mapped' => false signifie que ce champ n'est pas directement lié à une propriété de l'entité User
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter nos conditions.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                // au lieu d'être défini directement sur l'objet,
+                // ce champ est lu et encodé dans le contrôleur
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                        // longueur maximale autorisée par Symfony pour des raisons de sécurité
+                        'max' => 100,
+                        'maxMessage' => 'Votre mot de passe doit comporter au maximum {{ limit }} caractères',
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
+    /**
+     * Configure les options pour ce type de formulaire.
+     *
+     * @param OptionsResolver $resolver Le résolveur d'options.
+     */
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            // 'data_class' => User::class spécifie que ce formulaire sert à créer ou modifier des entités User
         ]);
     }
 }
